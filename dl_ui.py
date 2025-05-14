45963aa380351482f6c383ba4e6d4669444185f0
muhammad senna <muhamed.senna@gmai.com>
2025-05-14 22:23:13 +0300

    Add Gradio interface for DL model

--- dl_ui.py
+++ dl_ui.py
@@ -1,10 +1,36 @@
 import gradio as gr
+import tensorflow as tf
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as nb
 
-def dummy_dl_predict(text):
-    return "This is a placeholder DL model response."
+MAX_SEQUENCE_LENGTH = 100
+
+
+# Load model                        models/lstm_fragment_detection.h5
+model = tf.keras.models.load_model("models\lstm_fragment_detector.h5")
+
+# Load tokenizer
+with open("models\\tokenizer.pickle", "rb") as handle:
+    tokenizer = pickle.load(handle)
+
+
+def dl_predict(text):
+    # Tokenize and pad the input
+    sequence = tokenizer.texts_to_sequences([text])
+    padded = pad_sequences(sequence, maxlen=MAX_SEQUENCE_LENGTH, padding='post', truncating='post')
+    
+    
+    # Predict
+    prediction = model.predict(padded)[0][0]
+    
+    # Return label
+    label = "Fragment" if prediction > 0.5 else "Not a Fragment"
+    return f"{label} ({prediction:.2f})"
+
 
 dl_interface = gr.Interface(
-    fn=dummy_dl_predict,
+    fn=dl_predict,
     inputs=gr.Textbox(label="Enter text"),
     outputs=gr.Text(label="DL Model Prediction"),
     title="Fragment DL Classifier"
